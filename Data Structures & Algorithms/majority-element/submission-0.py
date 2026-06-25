358f24af10f1d3e7d45f3758e9133522989d64ef
Aayush Acharya <151422854+aayush4515@users.noreply.github.com>
2026-06-24 23:32:53 -0500

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        threshold = math.floor(len(nums) / 2)
+
+        freq_map = {}
+
+        for num in nums:
+            freq_map[num] = freq_map.get(num, 0) + 1
+        
+        largest_val = max(sorted(list(freq_map.values())))
+
+        for key, val in freq_map.items():
+            if val == largest_val:
+                return key
